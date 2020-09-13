c21b4b91b9098c4c73f191b1d96ffe8266519357
Kar-Den <k.denis.vlad@gmail.com>
2020-09-13 15:07:57 +0300

    task_1 объявите переменные всех простых(примитивных) типов. Проинициализируйте их значениями отличными от значений по умолчанию. Требования: 1. Переменные должны находиться внутри метода main 2. В методе main должны быть переменные всех простых типов 3. В методе main должны быть переменные только простых типов 4. Значения переменных должны отличаться от значений по умолчанию 5. Переменные должны иметь имя var + тип примитива без пробелов и подчеркиваний (например, переменная типа int должна называться varint)

---
+++ src/com/company/java0/part1/task1.java
@@ -0,0 +1,14 @@
+package com.company.java0.part1;
+
+public class task1 {
+    public static void main(String[] args) {
+        boolean varboolean = true;
+        int varint = 1;
+        long varlong = 1L;
+        float varfloat = 1.1F;
+        double vardouble = 2.5d;
+        short varshort = -3;
+        byte varbyte = -128;
+        char varchar = 'D';
+    }
+}
